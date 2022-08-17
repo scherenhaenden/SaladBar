6b7d122e540b8f6fb1080dc970506e5c8619130a
Eddie Krill <scherenhaenden@gmail.com>
2022-08-17 12:51:08 +0200

    Changes:
    - Show first Docker images via BackEndApi

--- SaladBarBackEnd/SaladBarBackEnd/Controllers/DockerController.cs
+++ SaladBarBackEnd/SaladBarBackEnd/Controllers/DockerController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using SaladBarBackend.DockerLib.Abstraction;
 
 namespace SaladBarBackEnd.Controllers;
 
@@ -6,10 +7,25 @@ namespace SaladBarBackEnd.Controllers;
 [Route("[controller]")]
 public class DockerController : Controller
 {
+    private readonly IDockerCommands _dockerCommands;
+
+    public DockerController(IDockerCommands dockerCommands)
+    {
+        _dockerCommands = dockerCommands;
+    }
+
     // GET
     [HttpGet(Name = "GetIndex")]
     public string Index()
     {
         return "Hello DockerController!";
+    }
+    
+    // GET List of all docker images  
+    [HttpGet]
+    [Route("GetImages")]
+    public async Task<List<DockerImageModel>> GetImages()
+    {
+        return await _dockerCommands.GetImages();
     }
 }
