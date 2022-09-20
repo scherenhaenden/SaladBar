7a04fc5596119efe73c490c3c03b6f982e13d425
scherenhaenden <scherenhaenden@gmail.com>
2022-09-20 13:32:25 +0200

    Refix

--- SaladBarFrontEnd/src/app/modules/docker/docker-modules/docker-volumes-list-view/docker-volumes-list-view.component.ts
+++ SaladBarFrontEnd/src/app/modules/docker/docker-modules/docker-volumes-list-view/docker-volumes-list-view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CardViewModel } from 'src/app/models/card-view-model';
+import { LinkTextPairModel } from 'src/app/models/link-text-pair-model';
+import { DockerCommonServicesService } from '../../services/docker-common-services.service';
+import { DockerVolume } from '../models/docker-volume';
 
 @Component({
   selector: 'app-docker-volumes-list-view',
@@ -7,9 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DockerVolumesListViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dockerCommonServicesService: DockerCommonServicesService) {
+    this.getDockerVolumes();
+  }
 
   ngOnInit(): void {
+  }
+
+  public cardView: CardViewModel[] = [];
+
+  // Get Draft of Load Docker Images
+  private async getDockerImages(): Promise<void> {
+
+    const result = await this.dockerCommonServicesService.getDockerVolumes();
+    console.log('result', result);
+
+    for (var image of result) {
+      const cardModel =  this.transFormDockerNetworkModelToCardViewModel(image);
+      this.cardView.push(cardModel);
+    }
+    //this.dockerImages = result;
+  }
+
+
+  private transFormDockerNetworkModelToCardViewModel(dockerVolume: DockerVolume): CardViewModel {
+    const cardView = new CardViewModel();
+    cardView.title = dockerVolume.name?.toString();
+
+    cardView.subtitle1 = dockerVolume.driver?.toString();
+    cardView.subtitle2 = dockerVolume.labels?.toString();
+
+    cardView.cardText1 = dockerVolume.mountpoint?.toString();
+    cardView.cardText2 = dockerVolume.usageData?.toString();
+    /*cardView.cardText3 = dockerImage.repoDigests?.toString();
+    cardView.cardText4 = dockerImage.sharedSize?.toString();
+    cardView.cardText5 = dockerImage.created?.toString();*/
+
+    const linkTextPairModel1: LinkTextPairModel = {
+      link : '#',
+      text: 'More Info'
+    };
+    const linkTextPairModel2: LinkTextPairModel = {
+      link : '#',
+      text: 'Edit'
+    };
+
+
+    cardView.cardLink1 = linkTextPairModel1;
+    cardView.cardLink2 = linkTextPairModel2;
+    return cardView;
   }
 
 }
