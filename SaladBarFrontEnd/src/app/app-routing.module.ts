40c4e672468fd04a037aca6c1fd04f00898014e6
Eddie Krill <scherenhaenden@gmail.com>
2022-10-16 12:03:05 +0200

    Changes:
    - Now home can be only used if the user is logged

--- SaladBarFrontEnd/src/app/app-routing.module.ts
+++ SaladBarFrontEnd/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './services/security/auth-guard.service';
 import { HomeViewComponent } from './views/home-view/home-view.component';
 import { LoginViewComponent } from './views/login-view/login-view.component';
 
 const routes: Routes = [
 
   { path: '', pathMatch: 'full', redirectTo: '/home' },
-  { path: 'home', component: HomeViewComponent  },
+  {
+    path: 'home', component: HomeViewComponent,
+    canActivate: [AuthGuardService]
+
+  },
   { path: 'login', component: LoginViewComponent  },
   { path: 'docker',
     loadChildren: () => import('./modules/docker/docker.module').then(m => m.DockerModule),
