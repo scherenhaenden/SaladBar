18d7e23da02698f004a253c6759d0bef9d76b21e
Eddie Krill <scherenhaenden@gmail.com>
2022-10-16 12:02:42 +0200

    Add guard service

--- SaladBarFrontEnd/src/app/services/security/auth-guard.service.ts
+++ SaladBarFrontEnd/src/app/services/security/auth-guard.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
+
+  constructor(private router: Router
+    , private authService: AuthService) { }
+  public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    if (this.authService.isLoggedIn()) {
+      // If the user is logged in, allow them to access the route
+      return true;
+    }
+    // If the user is not logged in, redirect them to the login page
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
+
+/*
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class CanActivateGuard implements CanActivate {
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ) : Observable<boolean> | Promise<boolean> | boolean {
+
+  }
+
+} {
 
   constructor() { }
-}
+}*/
+
